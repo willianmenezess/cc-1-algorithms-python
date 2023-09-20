17d4d5240ebc4307dc69baeb18ad57b23d2db777
willianmenezess <willmenezes@outlook.com.br>
2023-09-20 18:48:44 -0300

    refactor: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,9 +6,7 @@ def study_schedule(permanence_period, target_time):
     for e1, e2 in permanence_period:
         if not isinstance(e1, int) or not isinstance(e2, int):
             return
-        # if permanence[0] is None or permanence[1] is None:
-        #     return
-        if int(e1) <= target_time <= int(e2):
+        if e1 <= target_time <= e2:
             count_students += 1
     return count_students
 

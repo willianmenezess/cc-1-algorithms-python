7f9d3c69c979567852eed4edbbe354814913e02d
willianmenezess <willmenezes@outlook.com.br>
2023-09-21 15:31:59 -0300

    feat: resolve requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,19 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or word == '':
+        return False
+    low_index = 0
+    high_index = len(word) - 1
+
+    while high_index >= low_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
+
+
+if __name__ == '__main__':
+    word = 'socos'
+    result = is_palindrome_iterative(word)
+    print(result)
